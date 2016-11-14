f1e0077d742426c4a46a4e532336229294aeb58c
Nuno Anselmo <n.anselmo@hotmail.com>
2016-11-14 17:05:41 +0000

    Fixes previous commit

--- message_handler.py
+++ message_handler.py
@@ -35,27 +35,33 @@ class MessageHandler(object):
 
     @staticmethod
     def process():
-        '''Processes the next message in queue
-        If no message is in queue, it awaits until one is and then processes it'''
+        '''Processes the next message in queue. If no message is in queue,
+        it awaits until one is and then processes it'''
 
-        def new_file(file_object, path, timestamp):
-            '''Receives a file path, content, and timestamp, and writes said file'''
+        # TODO: Create login
+        def login(user, directory, obj_list):
+            '''Receives a user, directory, files/directories the sender has
+            and compares with what the receiver has, passing the
+            result as either requestFile or sendFile to the ObjectSocket'''
+            pass
+
+        def receive_object(file_object, path, timestamp):
+            '''Receives a file or directory object, its destination,
+            and desired timestamp'''
             file_object.set_timestamp(timestamp)
             file_object.move(path)
 
-        # TODO: Create get_files
-        def get_files():
-            '''Receives a path to a directory, returns all files inside'''
-            pass
+        # TODO: Finish core for send_object
+        def send_object(path):
+            '''Creates file/directory object and passes it to the ObjectSocket'''
+            if os.path.isdir(path):
+                obj = Directory(path)
+            else:
+                obj = File(path)
 
-        # TODO: Create get_descriptor
-        def get_descriptor():
-            '''Receives a path and returns a filedescriptor to that path'''
-            pass
-
-        # TODO: Create send_file
-        def send_file():
-            '''Sends the file in that path'''
+        # TODO: Finish core for send_object
+        def request_object(path):
+            '''Requests the file/directory object'''
             pass
 
         # TODO: Create logic for message processing
