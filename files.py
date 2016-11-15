573760db43e15982dbb61e8eb4b085f85a7e7ab0
Andre Mendes <patriqdesigns@gmail.com>
2016-11-15 18:53:12 +0000

    Prettify!

--- files.py
+++ files.py
@@ -1,11 +1,13 @@
-'''Wrapper for low-level OS calls regarding files and directories'''
+"""Wrapper for low-level OS calls regarding files and directories"""
 
 import os
 import shutil
 import tempfile
 
+
 class File(object):
     """Wrapper for low-level OS calls regarding files"""
+
     def __init__(self, path=None):
         super(File, self).__init__()
         if path is not None:
@@ -16,63 +18,62 @@ class File(object):
             self.file = open(temp[1], 'w+b')
 
     def get_path(self):
-        '''Returns the file's path'''
+        """Returns the file's path"""
         return self.file.name
 
-
     def get_relpath(self, path=None):
-        '''Returns the file's path relative to another one'''
+        """Returns the file's path relative to another one"""
         if path is None:
             path = os.curdir
         return os.path.relpath(self.get_path(), path)
 
     def get_timestamp(self):
-        '''Returns the modified timestamp'''
+        """Returns the modified timestamp"""
         return int(os.stat(self.get_path()).st_mtime)
 
     def move(self, destination):
-        '''Moves the file to the given path'''
+        """Moves the file to the given path"""
         self.file.close()
         os.makedirs(os.path.split(destination)[0])
         shutil.move(self.get_path(), destination)
         self.file = open(destination)
 
     def set_timestamp(self, timestamp):
-        '''Sets the modified and accessed timestamp to the given one'''
+        """Sets the modified and accessed timestamp to the given one"""
         os.utime(self.get_path(), (timestamp, timestamp))
 
     def set_position(self, position):
-        '''Sets the seek position'''
+        """Sets the seek position"""
         self.file.seek(position)
 
     def get_position(self):
-        '''Gets the current seek position'''
+        """Gets the current seek position"""
         return self.file.tell()
 
     def get_size(self):
-        '''Gets the size'''
+        """Gets the size"""
         return int(os.stat(self.get_path()).st_size)
 
     def write(self, data):
-        '''Writes data to file'''
+        """Writes data to file"""
         self.file.write(data)
 
     def flush(self):
-        '''Forcefully flushes pending data to file'''
+        """Forcefully flushes pending data to file"""
         self.file.flush()
 
     def read(self, count):
-        '''Reads data from the file'''
+        """Reads data from the file"""
         return self.file.read(count)
 
     def bytes(self):
-        '''Byte generator for this file'''
+        """Byte generator for this file"""
         for chunk in self.chunks(8192):
             for byte in chunk:
                 yield byte
 
     def chunks(self, chunksize=1024):
-        '''Chunk generator for this file'''
+        """Chunk generator for this file"""
         temp = self.get_position()
         self.set_position(0)
         while True:
@@ -83,8 +84,10 @@ class File(object):
                 break
         self.set_position(temp)
 
+
 class Directory(object):
     """Wrapper for low-level OS calls regarding directories"""
+
     def __init__(self, path=None):
         super(Directory, self).__init__()
         if path is not None:
@@ -93,32 +96,32 @@ class Directory(object):
             self.directory = tempfile.mkdtemp()
 
     def get_path(self):
-        '''Returns the directory's path'''
+        """Returns the directory's path"""
         return self.directory
 
     def get_relpath(self, path=None):
-        '''Returns the directory's path relative to another one'''
+        """Returns the directory's path relative to another one"""
         if path is None:
             path = os.curdir
         return os.path.relpath(self.get_path(), path)
 
     def get_timestamp(self):
-        '''Returns the modified timestamp'''
+        """Returns the modified timestamp"""
         return int(os.stat(self.get_path()).st_mtime)
 
     def move(self, destination):
-        '''Moves the directory to the given path'''
+        """Moves the directory to the given path"""
         destination = os.path.normpath(destination)
         os.makedirs(os.path.split(destination)[0])
         shutil.move(self.get_path(), destination)
         self.directory = destination
 
     def set_timestamp(self, timestamp):
-        '''Sets the modified and accessed timestamp to the given one'''
+        """Sets the modified and accessed timestamp to the given one"""
         os.utime(self.get_path(), (timestamp, timestamp))
 
     def list(self, recursive=False):
-        '''Generator for this directory, containing files and directories'''
+        """Generator for this directory, containing files and directories"""
         paths = os.listdir(self.get_path())
         for path in paths:
             abs_path = os.path.join(self.get_path(), path)
--- message_handler.py
+++ message_handler.py
@@ -1,9 +1,11 @@
-'''Handles all the core functions for PyBox'''
+"""Handles all the core functions for PyBox"""
 
-import threading
 import os
-from files import File, Directory
+import threading
+
 import packets
+from files import File, Directory
+
 
 class MessageHandler(object):
     """Handles all the core functions for PyBox"""
@@ -18,24 +20,24 @@ class MessageHandler(object):
             thread.start()
 
     def do_login(self, user, directory):
-        '''Creates a login packet and sends it to the ObjectSocket'''
+        """Creates a login packet and sends it to the ObjectSocket"""
         self.directory = Directory(directory)
         obj_list = []
         for f in self.directory.list():
-            obj_list.append(path=f.get_relpath(self.directory.get_path()) ,file_wrapper=packets.FileInfo(f))
+            obj_list.append(path=f.get_relpath(self.directory.get_path()), file_wrapper=packets.FileInfo(f))
         login_packet = packets.LoginPacket(user, os.path.split(directory)[1], obj_list)
         self.object_socket.send_object(login_packet)
 
     def process(self):
-        '''Processes the next message in queue. If no message is in queue,
-        it awaits until one is and then processes it'''
+        """Processes the next message in queue. If no message is in queue,
+        it awaits until one is and then processes it"""
 
         def receive_login(login_packet):
-            '''Receives a login packet and processes it, creating send_object
-            and request_object packets as needed to synchronize'''
+            """Receives a login packet and processes it, creating send_object
+            and request_object packets as needed to synchronize"""
 
             def request_object(path):
-                '''Creates a request file packet and sends it to the ObjectSocket'''
+                """Creates a request file packet and sends it to the ObjectSocket"""
                 info = packets.FileInfo(path=path)
                 request_file_packet = packets.RequestFilePacket(info)
                 self.object_socket.send_object(request_file_packet)
@@ -44,7 +46,7 @@ class MessageHandler(object):
             # TODO: Handle a new login, sending or requesting new files as needed
 
         def send_object(request_file_packet):
-            '''Creates a send object packet and sends it to the ObjectSocket'''
+            """Creates a send object packet and sends it to the ObjectSocket"""
             path = request_file_packet.file_info.path
             abs_path = os.path.join(self.directory.get_path(), path)
             if os.path.isdir(abs_path):
@@ -56,15 +58,15 @@ class MessageHandler(object):
             self.object_socket.send_object(send_file_packet)
 
         def receive_object(send_file_packet):
-            '''Receives a send file packet, and processes it'''
+            """Receives a send file packet, and processes it"""
             info = send_file_packet.file_info
             info.file_wrapper.set_timestamp(info.last_modified)
             info.file_wrapper.move(os.path.join(self.directory.get_path(), info.path))
 
         action = {
-            packets.LoginPacket:receive_login,
-            packets.RequestFilePacket:send_object,
-            packets.SendFilePacket:receive_object
+            packets.LoginPacket: receive_login,
+            packets.RequestFilePacket: send_object,
+            packets.SendFilePacket: receive_object
         }
 
         while True:
