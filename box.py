5d6042fd6cb90da972faf10079986d13c29de13b
Liliana0803 <liliana.oliveira.12@gmail.com>
2016-11-16 22:29:09 +0000

    box and server implementations

--- box.py
+++ box.py
@@ -2,6 +2,9 @@
 
 import message_handler
 import packets
+import object_socket
+import socket
+import sys
 
 # TODO: Write client main
 # Receive input, create socket to given hostname & port
@@ -11,6 +14,23 @@ import packets
 # Then, start processing by calling "process"
 def main():
     '''Starts execution once everything is loaded'''
-    pass
+    hostname = sys.argv[1]
+    port = sys.argv[2]
+    username = sys.argv[3]
+    directory = sys.argv[4]
+	
+	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
+    try:
+    	clientSocket.connect((hostname,port))
+	except Exception as e: 
+	    print("Desculpe mas o serviço de caixote não está disponível de momento")
+	finally:
+	    clientSocket.close()
+
+	objectSocket = object_socket.ObjectSocket(clientSocket)
+	messageHandler = message_handler.MessageHandler(objectSocket)
+	messageHandler.do_login(username,directory)
+	messageHandler.process()
+    
 main()
--- server.py
+++ server.py
@@ -2,6 +2,9 @@
 
 import message_handler
 import packets
+import object_socket
+import socket
+import sys
 
 # TODO: Write server main
 # Create socket listening on given port. When a connection is established:
@@ -10,6 +13,18 @@ import packets
 # Once that's done, listen on the given port once again
 def main():
     '''Starts execution once everything is loaded'''
-    pass
+    input = raw_input()
+    args= input.split(" ")
+   	hostname = sys.argv[1]
+    port = sys.argv[2]
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   	serverSocket.bind((‘’,port))
+   	serverSocket.listen(1)
+
+  
+	while 1:
+		connectionSocket = serverSocket.accept() 
+		objectSocket = object_socket.ObjectSocket(connectionSocket)
+		messageHandler = message_handler.MessageHandler(objectSocket,thread=True)
 
 main()
