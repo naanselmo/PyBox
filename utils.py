a088ae4035ce88d8b36f887d95f18874787a6d14
Nuno Anselmo <n.anselmo@hotmail.com>
2016-11-17 16:12:09 +0000

    Fixed timestamp errors

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ DEBUG_LEVEL = 0
 
 def log_message(code, message):
     """Formats and prints the given message"""
-    print '[' + format_timestamp(time.gmtime()) + ' - ' + code.center(6) + '] ' + str(message)
+    print '[' + format_timestamp(time.time()) + ' - ' + code.center(6) + '] ' + str(message)
 
 def format_timestamp(timestamp):
     """Formats a timestamp"""
-    return time.strftime("%H:%M:%S", timestamp)
+    return time.strftime("%H:%M:%S", time.localtime(timestamp))
